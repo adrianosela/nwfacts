aa2557254a86a661cfccdb2dc5a88eccf0d69301
Adriano <adriano.selaviles@gmail.com>
2020-01-12 02:45:15 -0800

    remove unnecessary healthcheck stuff

--- api/payloads/healthcheck.go
+++
@@ -1,7 +0,0 @@
-package payloads
-
-// HealthcheckResponse contains server health data
-type HealthcheckResponse struct {
-	Version    string `json:"version,omitempty"`
-	DeployTime string `json:"deployed_at"`
-}
