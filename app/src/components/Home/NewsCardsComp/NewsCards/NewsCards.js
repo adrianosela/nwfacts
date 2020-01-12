9fca80d3678729e4f9005a4da37c0f07d727e838
Adriano <adriano.selaviles@gmail.com>
2020-01-12 06:52:17 -0800

    styling

--- app/src/components/Home/NewsCardsComp/NewsCards/NewsCards.js
+++ app/src/components/Home/NewsCardsComp/NewsCards/NewsCards.js
@@ -4,17 +4,16 @@ import Divider from '@material-ui/core/Divider';
 
 import placeHolderIcon from "../../../../assets/newsIcon.png"
 class NewsCards extends React.Component {
-    
+
     getCategories = (cats) => {
         if (!cats) {return}
-        console.log('cats', cats)
         let res = ""
         for(var i=0; i<cats.length; i++) {
             res += cats[i].name + (i != cats.length-1 ?  ", " : "")
         }
-        return res
+        return "Categories: " + res
     }
-    
+
     render() {
         let source = this.props.source;
         return (
@@ -24,28 +23,25 @@ class NewsCards extends React.Component {
                 </div>
                 <div className="NewsCardDetails">
                     <div className="NewsCardTitle">{source.title}</div>
+                    <div className="NewsCardCategories">{this.getCategories(source.scores.categories)}</div>
                     <div className="NewsCardSource">Source: {source.source}</div>
                     <Divider orientation="horizontal" />
                     <div className="NewsCardDescription">{source.description}</div>
-                    <a href={source.url}>Full article here</a>
+                    <a href={source.url}>read more...</a>
                 </div>
                 <div className="NewsCardDividerVertical">
                     <Divider orientation="vertical" />
                 </div>
                 <div className="NewsCardScore">
                     <div className="MetricsTitle">
-                        Bias Metrics
+                        Bias Measurements
                     </div>
                     <Divider orientation="horizontal" />
                     <div className="MetricsContent">
                         <ul>
                             <li>Sensationalism: {source.scores.sensationalism}</li>
                             <li>Sentiment: {source.scores.sentiment}</li>
-                            <li>Categories: {this.getCategories(source.scores.categories)}</li>
                         </ul>
-                        <div className="NewsCardNote">
-                            Note: Lorem Ipsum fkshadk khfsdkajfhs khfsdkhfsda fhsdakhfsa
-                        </div>
                     </div>
                 </div>
             </div>
