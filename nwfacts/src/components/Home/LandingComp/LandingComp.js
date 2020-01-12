bc55d08873412879500746f56125edda7e17d082
Ali Mohamed <alihossam_10@live.com>
2020-01-11 22:47:57 -0800

    Centred everything on landing page

--- nwfacts/src/components/Home/LandingComp/LandingComp.js
+++ nwfacts/src/components/Home/LandingComp/LandingComp.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../LandingComp/SearchBar/SearchBar'
-
+import SearchComp from '../SearchComp/SearchComp'
 import "./LandingComp.css";
-import { Input, TextField } from '@material-ui/core'
+
 
 function LandingComp() {
 
-    return (
-      <div className="landingCompContainer">
-        <div className="titleContainter"> 
-          <div className="title">AggroNews</div>
-          <div className="titleDescriptor">Your News Aggregator!</div>
-        </div>
-        <div className="searchBarComp" >
-          <div className="spacerDiv1"></div>
-          <div className='searchbar'>
-            <form  noValidate autoComplete="off">   
-                <TextField id="outlined-basic" label="Search" variant="outlined" />
-            </form>
-          </div> 
-          <div className="spacerDiv1"></div>
-        </div>
+  return (
+    <div className="landingCompContainer">
+      <div className="titleContainter">
+        <div className="title">AggroNews</div>
+        <div className="titleDescriptor">Your News Aggregator!</div>
       </div>
-    )
+      <div className="searchBarContainer">
+        <SearchComp></SearchComp>
+      </div>
+    </div>
+  )
 }
 
 export default LandingComp;
---
+++ nwfacts/src/components/Home/SearchComp/SearchComp.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Input, TextField } from '@material-ui/core'
+import "./SearchComp.css";
+
+class SearchComp extends React.Component {
+    render() {
+        return <div className="searchBarComp" >
+                <form noValidate autoComplete="off" className="searchBoxContainer">
+                    <div className="searchBox">
+                        <TextField id="outlined-basic" label="Search" variant="outlined" fullWidth="true" />
+                    </div>
+                </form>
+                <Button variant="contained" color="primary">
+                        Go!
+                    </Button>
+        </div>
+    }
+}
+
+export default SearchComp;
