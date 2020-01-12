cb1a02b59890c0d4ae229711e10ed1ff74322897
Ali Mohamed <alihossam_10@live.com>
2020-01-11 18:39:47 -0800

    Fixed formatting in news parser go client

--- cloud-functions/newsparser/client/client.go
+++ cloud-functions/newsparser/client/client.go
@@ -1,12 +1,12 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
-	"net/http"
 	"fmt"
-    "encoding/json"
-    "bytes"
-    "io/ioutil"
+	"io/ioutil"
+	"net/http"
 )
 
 const FUNCTION_URL = "https://us-central1-nwfacts.cloudfunctions.net/ArticleParser"
@@ -14,34 +14,36 @@ const FUNCTION_URL = "https://us-central1-nwfacts.cloudfunctions.net/ArticlePars
 // Function that returns a map of URLs to Text Content
 // takes an array of urls and then makes a request to a service to get
 // the text content of articles from provided urls
-func GetTextFromArticles(urls string[]) (map[string]string, error) {
+func GetTextFromArticles(urls []string) (map[string]string, error) {
 	if len(urls) == 0 {
 		return nil, errors.New("Empty URLs array")
 	}
-	urlsMap := make(map[string][]string)
-	urlsMap["urls"] = urls
-	jsonUrls, err := json.Marshal(urlsMap)
+
+    urlmap := struct{
+        URLs string `json:"urls"`
+    }{URLs: urls}
+
+	jsonUrls, err := json.Marshal(&urlmap)
 	if err != nil {
-        fmt.Println(err)
+		fmt.Println(err)
 		return nil, errors.New(fmt.Sprintf("JSON could not be generated, %v", err))
 	}
-    res, err := http.Post(FUNCTION_URL, "application/json", bytes.NewBuffer(jsonUrls))
-    if err != nil {
-        fmt.Println(err)
-        return nil, errors.New("Error in http request")
-    }
-    body, err := ioutil.ReadAll(res.Body)
+	res, err := http.Post(FUNCTION_URL, "application/json", bytes.NewBuffer(jsonUrls))
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Error in http request")
+	}
+	body, err := ioutil.ReadAll(res.Body)
 
-    if err != nil {
-        fmt.Println(err)
-        return nil, errors.New("Error in reading response body")
-    }
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Error in reading response body")
+	}
 	var returnedUrlsMap map[string]string
 	defer res.Body.Close()
-    err = json.Unmarshal([]byte(body), &returnedUrlsMap)
-    if err != nil {
-        return nil, errors.New("Error in reading response from JSON")
-    }
+	err = json.Unmarshal([]byte(body), &returnedUrlsMap)
+	if err != nil {
+		return nil, errors.New("Error in reading response from JSON")
+	}
 	return returnedUrlsMap, nil
 }
-
