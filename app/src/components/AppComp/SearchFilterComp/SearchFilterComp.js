c9fde8a518c34efef89add29542a1206337629d7
rizvi95 <33403076+rizvi95@users.noreply.github.com>
2020-01-12 12:30:59 -0800

    please work

--- app/src/components/AppComp/SearchFilterComp/SearchFilterComp.js
+++ app/src/components/AppComp/SearchFilterComp/SearchFilterComp.js
@@ -12,14 +12,16 @@ class SearchFilterComp extends Component{
     constructor(props) {
         super(props)
     }
-    onGo = async () => {
+    onGo = async (param) => {
         console.log('\n\nsiiiii\n', this.props.results.results, typeof [this.props.results.results])
         let obj = []
+        let count = 0;
         this.props.results.results.forEach((item) => {
+            if (count <= 5) { 
             let temp = {
                 "source": item.source,
                 "publishedAt":item.publishedAt, 
-                "title":item.title, 
+                "title":item.title.slice(0,10), 
                 "url":item.url, 
                 "scores": {
                     "sentiment": item.scores.sentiment,
@@ -27,9 +29,10 @@ class SearchFilterComp extends Component{
                 }
             }
             obj.push(temp)
+            count++;
+        }
         })
-        console.log("ITEM", obj)
-
+        console.log("ITEM", obj)       
         try{
             const res = await fetch(`https://febg.api.stdlib.com/http-project1@dev/export-slack/?data=${JSON.stringify(obj)}`, {
                 method: 'GET',
@@ -38,13 +41,59 @@ class SearchFilterComp extends Component{
                   'Content-Type': 'application/json'
                 },
               });
-            const results = await res.json();
-            this.setState({ redirect: true, results: results});
+            // const results = await res.json();
+            // this.setState({ redirect: true, results: results});
         } catch (e) {
             console.log("ERROR ", e)
         }
-
     }
+
+    onGo2 = async (emailProp) => {
+        console.log('\n\nsiiiii\n', this.props.results.results, typeof [this.props.results.results])
+        let obj = []
+        let count = 0
+        this.props.results.results.forEach((item) => {
+            if (count <= 5) { 
+                let temp = {
+                    "source": item.source,
+                    "publishedAt":item.publishedAt, 
+                    "title":item.title.slice(0,10), 
+                    "url":item.url, 
+                    "scores": {
+                        "sentiment": item.scores.sentiment,
+                        "sensationalism": item.scores.sensationalism
+                    }
+                }
+                obj.push(temp)
+                count++;
+            }
+        })
+        console.log("ITEM", obj)       
+        try{
+            console.log(emailProp)
+            const res = await fetch(`https://febg.api.stdlib.com/http-project1@dev/export-email/?data=${JSON.stringify(obj)}&email=${emailProp}`, {
+                method: 'GET',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+              });
+            // const results = await res.json();
+            // this.setState({ results: results});
+        } catch (e) {
+            console.log("ERROR ", e)
+        }
+    }
+
+    loadExportHandler(slackProp, emailProp){
+        if(slackProp == 'on'){
+            this.onGo(slackProp);
+        }
+        if(emailProp != null){
+            this.onGo(emailProp);
+        }
+    }
+
     render() {
         return(
             <div className="SearchFilterCompContainer">
@@ -53,7 +102,7 @@ class SearchFilterComp extends Component{
                         <SearchComp />
                     </div>
                     <div className='exportButton'>
-                        <ExportButton />
+                        <ExportButton onfinishSlack={this.onGo} onfinishEmail={this.onGo2}/>
                     </div>
                 </div>
                 {/* <div className='exportButton'>
