39e98f8acd49f1022955978235df9e3766dd4c11
Adriano <adriano.selaviles@gmail.com>
2020-01-11 18:25:51 -0800

    processing results

---
+++ api/processing/result.go
@@ -0,0 +1,14 @@
+package processing
+
+// Result represents a processing result
+type Result struct {
+	// article metadata
+	Source      string `json:"source"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+	PublishedAt string `json:"publishedAt"`
+	Author      string `json:"author,omitempty"`
+	// article scores for different named scoring functions
+	Scores map[string]int `json:"scores"`
+}
