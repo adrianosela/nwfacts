6074fa12eed67f29be38b8dfce31a3a15313511e
Adriano <adriano.selaviles@gmail.com>
2020-01-12 09:01:05 -0800

    graphs

--- app/src/components/Home/NewsCardsComp/NewsCards/NewsCards.js
+++ app/src/components/Home/NewsCardsComp/NewsCards/NewsCards.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import "./NewsCards.css";
 import Divider from '@material-ui/core/Divider';
-
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import placeHolderIcon from "../../../../assets/newsIcon.png"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 class NewsCards extends React.Component {
 
     getCategories = (cats) => {
@@ -14,9 +17,34 @@ class NewsCards extends React.Component {
         return "Categories: " + res
     }
 
+    getSentimentGraph = (score) => {
+        if (score > 0) {
+            return <ProgressBar animated variant="warning" now={Math.abs(score*100)} />
+        }
+        else {
+            return <ProgressBar animated variant="danger" now={Math.abs(score*100)} />
+        }
+    }
+
+    getSensationalismGraph = (score) => {
+        if (score < 0.01) {
+            return <ProgressBar animated variant="success" now={Math.abs(score*1000)} />
+        }
+        else if (score < 0.05) {
+            return <ProgressBar animated variant="info" now={Math.abs(score*1000)} />
+        }
+        else if (score < 0.08) {
+            return <ProgressBar animated variant="warning" now={Math.abs(score*1000)} />
+        }
+        else {
+            return <ProgressBar animated variant="danger" now={Math.abs(score*1000)} />
+        }
+    }
+
     render() {
         let source = this.props.source;
         return (
+
             <div className="NewsCardContainer">
                 <div className="ImageContainer">
                     <img src={placeHolderIcon} alt="News source icon" className="NewsCardLogo" />
@@ -34,13 +62,15 @@ class NewsCards extends React.Component {
                 </div>
                 <div className="NewsCardScore">
                     <div className="MetricsTitle">
-                        Bias Measurements
+                        Bias Metrics
                     </div>
                     <Divider orientation="horizontal" />
                     <div className="MetricsContent">
                         <ul>
                             <li>Sensationalism: {source.scores.sensationalism}</li>
+                            {this.getSensationalismGraph(source.scores.sensationalism)}
                             <li>Sentiment: {source.scores.sentiment}</li>
+                            {this.getSentimentGraph(source.scores.sentiment)}
                         </ul>
                     </div>
                 </div>
