5a206d594beb2d4c67f69b2d565ae9412950c412
Ali Mohamed <alihossam_10@live.com>
2020-01-11 23:13:07 -0800

    Added newspaper icon to landing page

--- nwfacts/src/components/Home/LandingComp/LandingComp.js
+++ nwfacts/src/components/Home/LandingComp/LandingComp.js
@@ -4,14 +4,16 @@ import SearchBar from '../LandingComp/SearchBar/SearchBar'
 import SearchComp from '../SearchComp/SearchComp'
 import "./LandingComp.css";
 import Icon from "../../../assets/chevron.png"
+import NewsIcon from "../../../assets/newsIcon.png"
 
 function LandingComp() {
 
   return (
     <div className="landingCompContainer">
       <div className="titleContainter">
-        <div className="title">AggroNews</div>
-        <div className="titleDescriptor">Your News Aggregator!</div>
+        <div className="title">AggroNews <img src={NewsIcon} className="newsIcon"/></div>
+        <div className="titleDescriptor">Your Fake News Analyser!</div>
+        
       </div>
       <div className="searchBarContainer">
         <SearchComp></SearchComp>
